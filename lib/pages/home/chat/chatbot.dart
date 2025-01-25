ec05d54ce94ea9c9ffe7b2481eabb8ee1b37836e
Alif-Fikri <aliefmaulanafikri@gmail.com>
2025-01-25 23:56:41 +0700

    remove screen size chatbot

--- lib/pages/home/chat/chatbot.dart
+++ lib/pages/home/chat/chatbot.dart
@@ -7,9 +7,6 @@ class ChatbotOpeningPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final screenWidth = MediaQuery.of(context).size.width;
-    final screenHeight = MediaQuery.of(context).size.height;
-
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -18,8 +15,8 @@ class ChatbotOpeningPage extends StatelessWidget {
         leading: IconButton(
           icon: Image.asset(
             'assets/images/close_icon.png',
-            width: screenWidth * 0.04,
-            height: screenHeight * 0.04,
+            width: 17,
+            height: 17,
           ),
           onPressed: () {
             Navigator.pop(context);
@@ -29,14 +26,14 @@ class ChatbotOpeningPage extends StatelessWidget {
       body: Stack(
         children: [
           Positioned(
-            bottom: screenHeight * 0.20,
+            bottom: 250,
             left: 0,
             right: 0,
             child: Center(
               child: Container(
                 constraints: BoxConstraints(
-                  maxWidth: screenWidth * 1.0,
-                  maxHeight: screenHeight * 1.0,
+                  maxWidth: 700,
+                  maxHeight: 350,
                 ),
                 decoration: BoxDecoration(
                   image: DecorationImage(
@@ -57,7 +54,7 @@ class ChatbotOpeningPage extends StatelessWidget {
                         "Hallo\nAku Suki",
                         textAlign: TextAlign.center,
                         style: GoogleFonts.inter(
-                          fontSize: screenWidth * 0.07,
+                          fontSize: 28,
                           fontWeight: FontWeight.bold,
                           color: const Color(0xff2A3342),
                         ),
@@ -65,8 +62,8 @@ class ChatbotOpeningPage extends StatelessWidget {
                       const SizedBox(height: 10),
                       Image.asset(
                         'assets/images/suki.png',
-                        width: screenWidth * 0.4,
-                        height: screenHeight * 0.3,
+                        width: 150,
+                        height: 150,
                       ),
                     ],
                   ),
@@ -74,7 +71,7 @@ class ChatbotOpeningPage extends StatelessWidget {
                     "Asisten anda untuk \nmendeteksi banjir \ndan kerumunan",
                     textAlign: TextAlign.center,
                     style: GoogleFonts.inter(
-                      fontSize: screenWidth * 0.05,
+                      fontSize: 18,
                       color: const Color(0xff2A3342),
                       fontWeight: FontWeight.w600,
                     ),
@@ -93,15 +90,15 @@ class ChatbotOpeningPage extends StatelessWidget {
                         borderRadius: BorderRadius.circular(10),
                       ),
                       padding: EdgeInsets.symmetric(
-                        horizontal: screenWidth * 0.1,
-                        vertical: screenHeight * 0.03,
+                        horizontal: 40,
+                        vertical: 12,
                       ),
                     ),
                     child: Text(
                       "Start New Chat",
                       style: GoogleFonts.inter(
                         color: Colors.white,
-                        fontSize: screenWidth * 0.035,
+                        fontSize: 14,
                         fontWeight: FontWeight.w600,
                       ),
                     ),
@@ -112,7 +109,7 @@ class ChatbotOpeningPage extends StatelessWidget {
                     backgroundColor: const Color(0xffEAEFF3),
                     child: Icon(
                       Icons.mic,
-                      size: screenWidth * 0.06,
+                      size: 30,
                       color: Colors.red,
                     ),
                   ),
