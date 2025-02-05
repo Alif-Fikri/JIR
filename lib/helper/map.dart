a5806647617c7f1c6e4991f5e5684f5e5c558ba6
Alif-Fikri <aliefmaulanafikri@gmail.com>
2025-02-05 23:54:11 +0700

    make location and map to be reuseable

---
+++ lib/helper/map.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:smartcitys/helper/radar_map.dart';
+
+class ReusableMap extends StatefulWidget {
+  final LatLng initialLocation;
+  final List<Marker> markers;
+  final Function(LatLng)? onLocationChanged;
+  final Function(MapController)? onMapCreated;
+
+  const ReusableMap({
+    super.key,
+    required this.initialLocation,
+    required this.markers,
+    this.onLocationChanged,
+    this.onMapCreated,
+  });
+
+  @override
+  _ReusableMapState createState() => _ReusableMapState();
+}
+
+class _ReusableMapState extends State<ReusableMap> {
+  late MapController _mapController;
+  LatLng? _currentLocation;
+
+  @override
+  void initState() {
+    super.initState();
+    _mapController = MapController();
+    _checkLocationPermission();
+  }
+
+  Future<void> _checkLocationPermission() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission != LocationPermission.whileInUse && 
+          permission != LocationPermission.always) {
+        return;
+      }
+    }
+    _getCurrentLocation();
+  }
+
+  Future<void> _getCurrentLocation() async {
+    Position position = await Geolocator.getCurrentPosition();
+    setState(() {
+      _currentLocation = LatLng(position.latitude, position.longitude);
+    });
+    widget.onLocationChanged?.call(_currentLocation!);
+    _mapController.move(_currentLocation!, 15.0);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FlutterMap(
+      mapController: _mapController,
+      options: MapOptions(
+        initialCenter: widget.initialLocation,
+        initialZoom: 15.0,
+      ),
+      children: [
+        TileLayer(
+          urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+          subdomains: ['a', 'b', 'c'],
+        ),
+        MarkerLayer(markers: widget.markers),
+      ],
+    );
+  }
+}
