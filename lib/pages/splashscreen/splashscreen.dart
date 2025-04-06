6f0ba98df4d48911cd386284e077eb8b11aaefab
Alif-Fikri <aliefmaulanafikri@gmail.com>
2025-04-06 10:15:02 +0700

    fix animation transisi splash screen

--- lib/pages/splashscreen/splashscreen.dart
+++ lib/pages/splashscreen/splashscreen.dart
@@ -10,33 +10,90 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
-  bool showSecondSplash = false;
+class _SplashScreenState extends State<SplashScreen>
+    with TickerProviderStateMixin {
+  bool showBlueBackground = false;
+
+  late AnimationController _waveController;
 
   @override
   void initState() {
     super.initState();
+    _waveController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    )..addListener(() {
+        if (_waveController.value >= 0.5 && mounted) {
+          setState(() {});
+        }
+      });
+
     _startSplashSequence();
   }
 
   void _startSplashSequence() async {
     await Future.delayed(const Duration(seconds: 2));
-    setState(() {
-      showSecondSplash = true;
-    });
+    _waveController.forward();
+    setState(() => showBlueBackground = true);
     await Future.delayed(const Duration(seconds: 2));
     Get.off(() => AppRoutes.home);
   }
 
   @override
-Widget build(BuildContext context) {
-  return Scaffold(
-    backgroundColor: showSecondSplash ? const Color(0xFF0069C0) : Colors.white,
-    body: AnimatedSwitcher(
-      duration: const Duration(milliseconds: 500),
-      child: showSecondSplash
-          ? Stack(
-              key: const ValueKey(2),
+  void dispose() {
+    _waveController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          if (_waveController.value < 0.5)
+            Center(
+              child: Image.asset(
+                'assets/images/jir_logo.png',
+                width: 200,
+                height: 200,
+              ),
+            ),
+          if (showBlueBackground)
+            AnimatedBuilder(
+              animation: _waveController,
+              builder: (context, child) {
+                return Positioned(
+                  right: -screenSize.width * 0.5,
+                  bottom: -screenSize.height * 0.5,
+                  child: Transform.rotate(
+                    angle: 9.0,
+                    child: ClipPath(
+                      clipper: ComplexWaveClipper(_waveController.value),
+                      child: Container(
+                        width: screenSize.width * 2,
+                        height: screenSize.height * 2,
+                        decoration: const BoxDecoration(
+                          gradient: LinearGradient(
+                            begin: Alignment.topLeft,
+                            end: Alignment.bottomRight,
+                            stops: [0.0, 0.76],
+                            colors: [
+                              Color(0xFFA0CFEE),
+                              Color(0xFF004E92),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          if (_waveController.value >= 0.5)
+            Stack(
               children: [
                 Center(
                   child: Image.asset(
@@ -60,16 +117,59 @@ Widget build(BuildContext context) {
                   ),
                 ),
               ],
-            )
-          : Center(
-              key: const ValueKey(1),
-              child: Image.asset(
-                'assets/images/jir_logo.png',
-                width: 200,
-                height: 200,
-              ),
             ),
-    ),
-  );
+        ],
+      ),
+    );
+  }
 }
+
+class ComplexWaveClipper extends CustomClipper<Path> {
+  final double progress;
+
+  ComplexWaveClipper(this.progress);
+
+  @override
+  Path getClip(Size size) {
+    final path = Path();
+    final heightProgress = size.height * progress;
+
+    path.lineTo(0, heightProgress - 80);
+
+    path.cubicTo(
+      size.width * 0.1,
+      heightProgress + 20,
+      size.width * 0.2,
+      heightProgress - 100,
+      size.width * 0.3,
+      heightProgress - 20,
+    );
+
+    path.cubicTo(
+      size.width * 0.4,
+      heightProgress + 60,
+      size.width * 0.5,
+      heightProgress - 100,
+      size.width * 0.6,
+      heightProgress - 10,
+    );
+
+    path.cubicTo(
+      size.width * 0.7,
+      heightProgress + 40,
+      size.width * 0.8,
+      heightProgress - 120,
+      size.width,
+      heightProgress - 40,
+    );
+
+    path.lineTo(size.width, 0);
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(ComplexWaveClipper oldClipper) =>
+      oldClipper.progress != progress;
 }
